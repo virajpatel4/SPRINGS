17eff736129b6be855348c3440c21cf50a7ea781
virajpatel4 <VP97@myscc.ca>
2022-10-16 22:16:01 -0400

    Initial commit

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,13 +13,16 @@ namespace Springs.Controllers
         {
             return View();
         }
-       /* public string Index()
+        /* public string Index()
+         {
+             return "This is my default action...";
+         }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return "This is my default action...";
-        }*/
-        public string Welcome(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
 
     }
---
+++ Models/Springs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Springs.Models
+{
+    public class Springs
+    {
+        public int Id { get; set; }
+
+        public int Model { get; set; }
+        public string Color { get; set; }
+
+        public string Material { get; set; }
+
+        //[DataType(DataType.Date)]
+        public DateTime ManufacturingDate { get; set; }
+        //public int Model { get; set; }
+        public decimal Price { get; set; }
+
+        public string IndustryName { get; set; }
+
+        //public string Material { get; set; }
+    }
+}
