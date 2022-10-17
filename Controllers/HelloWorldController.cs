cc308d7ef8b5059d7e757cca959c501d0b5d12d0
virajpatel4 <VP97@myscc.ca>
2022-10-16 21:55:11 -0400

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Springs.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       /* public string Index()
+        {
+            return "This is my default action...";
+        }*/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
